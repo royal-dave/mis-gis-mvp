a4174349fc30ca81a50951254be1e170f1042d66
royal-dave <davidnsscdcl@gmail.com>
2026-01-31 10:01:26 +0530

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,1 +1,11 @@
-
+def check_user(username, password):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT username, role FROM users WHERE username=%s AND password=%s",
+        (username, password)
+    )
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+    return row  # None or (username, role)
